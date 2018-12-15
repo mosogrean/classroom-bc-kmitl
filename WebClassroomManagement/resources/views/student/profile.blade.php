37b036c7d4d137b46cf6195040b1eb71af498da2
James <jame555jame125@gmail.com>
2018-12-16 04:49:52 +0700

    [WEB]student-profile-create

--- WebClassroomManagement/resources/views/student/profile.blade.php
+++ WebClassroomManagement/resources/views/student/profile.blade.php
@@ -5,7 +5,7 @@
         <div class="row justify-content-center">
             <div class="col-md-8">
                 <div class="card">
-                    <div class="card-header">Student role</div>
+                    <div class="card-header">Student Profile</div>
 
                     <div class="card-body">
                         @if (session('status'))
@@ -13,6 +13,14 @@
                                 {{ session('status') }}
                             </div>
                         @endif
+                        <ul class="list-group">
+                            <li class="list-group-item active">Student ID</li>
+                            <li class="list-group-item">Name</li>
+                            <li class="list-group-item">Tel.</li>
+                            <li class="list-group-item">E-mail</li>
+                            <li class="list-group-item">Token</li>
+                            <li class="list-group-item">Room</li>
+                        </ul>
                     </div>
                 </div>
             </div>
--- WebClassroomManagement/routes/web/student_route.php
+++ WebClassroomManagement/routes/web/student_route.php
@@ -1,3 +1,4 @@
 <?php
 
 Route::get('/student/dashboard','Student\DashboardController@index')->name('student.dashboard');
+Route::get('/student/profile','Student\ProfileController@index')->name('student.profile');
